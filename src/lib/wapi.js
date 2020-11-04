49ba4b26f9efe0dca91549f8b7ba7a9ff6a1f897
Julio Vaz <jmatheus.vaz@gmail.com>
2020-11-03 21:43:39 -0300

    uncomment observable

--- src/lib/wapi.js
+++ src/lib/wapi.js
@@ -2221,3 +2221,81 @@ window.WAPI.addLicenseKey = async function (key){
       const x = await r.text()
       return eval(x);
     }
+
+window.addEventListener("unload", window.WAPI._unloadInform, false);
+window.addEventListener("beforeunload", window.WAPI._unloadInform, false);
+window.addEventListener("pageunload", window.WAPI._unloadInform, false);
+
+/**
+ * Registers a callback to be called when a new message arrives the WAPI.
+ * @param rmCallbackAfterUse - Boolean - Specify if the callback need to be executed only once
+ * @param done - function - Callback function to be called when a new message arrives.
+ * @returns {boolean}
+ */
+window.WAPI.waitNewMessages = function(rmCallbackAfterUse = true, done) {
+  window.WAPI._newMessagesCallbacks.push({callback: done, rmAfterUse: rmCallbackAfterUse});
+  return true;
+};
+
+/**
+ * Reads buffered new messages.
+ * @param done - function - Callback function to be called contained the buffered messages.
+ * @returns {Array}
+ */
+window.WAPI.getBufferedNewMessages = function(done) {
+  let bufferedMessages = window.WAPI._newMessagesBuffer;
+  window.WAPI._newMessagesBuffer = [];
+  if(done !== undefined) {
+    done(bufferedMessages);
+  }
+  return bufferedMessages;
+};
+
+
+/**
+ * New messages observable functions.
+ */
+window.WAPI._newMessagesQueue = [];
+window.WAPI._newMessagesBuffer = (sessionStorage.getItem('saved_msgs') != null) ?
+  JSON.parse(sessionStorage.getItem('saved_msgs')) : [];
+window.WAPI._newMessagesDebouncer = null;
+window.WAPI._newMessagesCallbacks = [];
+window.Store.Msg.off('add');
+sessionStorage.removeItem('saved_msgs');
+
+window.WAPI._newMessagesListener = window.Store.Msg.on('add', (newMessage) => {
+  if (newMessage && newMessage.isNewMsg && !newMessage.isSentByMe) {
+    let message = window.WAPI.processMessageObj(newMessage, false, false);
+    if (message) {
+      window.WAPI._newMessagesQueue.push(message);
+      window.WAPI._newMessagesBuffer.push(message);
+    }
+
+    // Starts debouncer time to don't call a callback for each message if more than one message arrives
+    // in the same second
+    if(!window.WAPI._newMessagesDebouncer && window.WAPI._newMessagesQueue.length > 0) {
+      window.WAPI._newMessagesDebouncer = setTimeout(() => {
+        window.WAPI._newMessagesDebouncer = null;
+        let queuedMessages = window.WAPI._newMessagesQueue;
+        window.WAPI._newMessagesQueue = [];
+
+        let removeCallbacks = [];
+        window.WAPI._newMessagesCallbacks.forEach(function(callbackObj) {
+          if(callbackObj.callback !== undefined) {
+            callbackObj.callback(queuedMessages);
+          }
+          if(callbackObj.rmAfterUse === true) {
+            removeCallbacks.push(callbackObj);
+          }
+        });
+
+        // Remove removable callbacks.
+        removeCallbacks.forEach(function(rmCallbackObj) {
+          let callbackIndex = window.WAPI._newMessagesCallbacks.indexOf(rmCallbackObj);
+          window.WAPI._newMessagesCallbacks.splice(callbackIndex, 1);
+        });
+      }, 1000);
+    }
+  }
+})
+/** End new messages observable functions **/
