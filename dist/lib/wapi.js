584802d08a08882713fef92f4d0fc517f9f80d63
Julio Vaz <jmatheus.vaz@gmail.com>
2020-11-23 14:16:48 -0300

    base64

--- dist/lib/wapi.js
+++ dist/lib/wapi.js
@@ -1506,6 +1506,22 @@ window.WAPI.sendImage = async function (imgBase64, chatid, filename, caption, qu
     return newId._serialized;
 }
 
+window.WAPI.base64ToFile = function(base64, filename) {
+  var arr = base64.split(',');
+  var mime = arr[0].match(/:(.*?);/)[1];
+  var bstr = window.Base64 ? window.Base64.atob(arr[1]) : atob(arr[1]);
+  var n = bstr.length;
+  var u8arr = new Uint8Array(n);
+
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+
+  return new File([u8arr], filename, {
+    type: mime,
+  });
+}
+
 window.WAPI.sendFile = async function(imgBase64, chatid, filename, caption, type) {
   type = type ? type : 'sendFile';
 
