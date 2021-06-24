4a9d433d66ecf3781493a5a3c41ecc6318a9f0cc
Julio Vaz <jmatheus.vaz@gmail.com>
2021-06-24 01:34:47 -0300

    run build

--- dist/lib/wapi.js
+++ dist/lib/wapi.js
@@ -2087,7 +2087,7 @@ window.WAPI.reply = async function (chatId, body, quotedMsg) {
     ...quotedMsgOptions,
   };
 
-  //await Promise.all(window.Store.addAndSendMsgToChat(chat, message));
+  await Promise.all(window.Store.addAndSendMsgToChat(chat, message));
   return newId._serialized;
 };
 
