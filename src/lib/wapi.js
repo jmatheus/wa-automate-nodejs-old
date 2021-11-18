4b020dabc7a22077b03274607f5b4794fffa19a0
Julio Matheus Vaz <jmatheus.vaz@gmail.com>
2021-11-18 17:33:36 -0300

    updating wapi scope code

--- src/lib/wapi.js
+++ src/lib/wapi.js
@@ -1558,45 +1558,43 @@ window.WAPI.clearChat = async function (id) {
 //  }
 //}
 
-window.WAPI.checkNumberStatus = async function (id) {
+window.WAPI.checkNumberStatus = async function (id, conn = true) {
   try {
-    const result = await window.WAPI.findJidFromNumber(id);
-    if (result.jid === undefined) throw 404;
-    const data = window.WAPI._serializeNumberStatusObj(result);
-    if (data.status == 200) data.numberExists = true
-    return data;
+    const err = { error: 404 };
+    const checkType = WAPI.sendCheckType(id);
+    if (!!checkType && checkType.status === 404) {
+      Object.assign(err, { text: checkType.text });
+      throw err;
+    }
+
+    if (conn === true) {
+      const connection = window.Store.State.default.state;
+      if (connection !== 'CONNECTED') {
+        Object.assign(err, {
+          text: 'No connection with WhatsApp',
+          connection: connection
+        });
+        throw err;
+      }
+    }
+
+    const result = await Store.checkNumber(id);
+    if (!!result && typeof result === 'object') {
+      const data = {
+        status: 200,
+        numberExists: true,
+        id: result.wid
+      };
+      return data;
+    }
+
+    throw err;
   } catch (e) {
-    return window.WAPI._serializeNumberStatusObj({
-      status: e,
-      jid: id
-    });
+    return {
+      status: e.error,
+      text: e.text
+    };
   }
-};
-
-window.WAPI.findJidFromNumber = async function (number) {
-  if (window.WAPI.isMultiDeviceVersion()) {
-    return Store.QueryExist.queryExist(WAPI.tryFixNumber(number)).then(value => {
-      return { status: 200, jid: value.wid }
-    });
-  } else {
-    if (!number.includes("@c.us"))
-      number += "@c.us";
-    return Store.WapQuery.queryExist(number);
-  }
-}
-
-window.WAPI.tryFixNumber = async function (number) {
-  let firstNumbersMe = Store.GetMaybeMeUser.getMaybeMeUser().user.substring(0, 2);
-  let firstNumbersContact = number.substring(0, 2);
-  if (firstNumbersMe === firstNumbersContact) {
-    return number.substring(2);
-  }
-  firstNumbersMe = Store.GetMaybeMeUser.getMaybeMeUser().user.substring(0, 3);
-  firstNumbersContact = number.substring(0, 3);
-  if (firstNumbersMe === firstNumbersContact) {
-    return number.substring(3);
-  }
-  return number;
 }
 
 window.WAPI.onAnyMessage = callback => window.Store.Msg.on('add', (newMessage) => {
@@ -2019,62 +2017,178 @@ window.WAPI.sendFile = async function(imgBase64, chatid, filename, caption, type
   }
 }
 
-window.WAPI.scope = function(id, erro, status, text = null) {
-  let e = {
+window.WAPI.scope = function(id, erro, status, text = null, result = null) {
+  const object = {
     me: Store.Me.attributes,
     to: id,
     erro: erro,
     text: text,
     status: status,
+    result: result
   };
-  return e;
+  return object;
 }
 
 window.WAPI.getchatId = async function(chatId) {
-  var to = await WAPI.getChatById(chatId),
-    objTo = to.lastReceivedKey,
-    extend = {
-      formattedName: to.contact.formattedName,
-      isBusiness: to.contact.isBusiness,
-      isMyContact: to.contact.isMyContact,
-      verifiedName: to.contact.verifiedName,
-      pushname: to.contact.pushname,
-      isOnline: to.isOnline,
-    };
-  Object.assign(objTo, extend);
-  return objTo;
+  if (chatId) {
+    let to = await WAPI.getChatById(chatId);
+    if (to && typeof to === 'object') {
+      let objTo = to.lastReceivedKey;
+      if (objTo && typeof objTo === 'object') {
+        let extend = {
+          formattedName: to.contact.formattedName,
+          isBusiness: to.contact.isBusiness,
+          isMyContact: to.contact.isMyContact,
+          verifiedName: to.contact.verifiedName,
+          pushname: to.contact.pushname,
+          isOnline: to.isOnline
+        };
+        Object.assign(objTo, extend);
+        return objTo;
+      }
+    }
+  }
+  return undefined;
+}
+
+window.WAPI.sendCheckType = async function(chatId = undefined) {
+  if (!chatId) {
+    return WAPI.scope(chatId, true, 404, 'It is necessary to pass a number!');
+  }
+  if (typeof chatId === 'string') {
+    const contact = '@c.us';
+    const broadcast = '@broadcast';
+    const grup = '@g.us';
+    if (
+      contact !== chatId.substr(-contact.length, contact.length) &&
+      broadcast !== chatId.substr(-broadcast.length, broadcast.length) &&
+      grup !== chatId.substr(-grup.length, grup.length)
+    ) {
+      return WAPI.scope(
+        chatId,
+        true,
+        404,
+        'The chat number must contain the parameters @c.us, @broadcast or @g.us. At the end of the number!'
+      );
+    }
+    if (
+      contact === chatId.substr(-contact.length, contact.length) &&
+      ((chatId.match(/(@c.us)/g) && chatId.match(/(@c.us)/g).length > 1) ||
+        !chatId.match(/^(\d+(\d)*@c.us)$/g))
+    ) {
+      return WAPI.scope(
+        chatId,
+        true,
+        404,
+        'incorrect parameters! Use as an example: 000000000000@c.us'
+      );
+    }
+
+    if (
+      broadcast === chatId.substr(-broadcast.length, broadcast.length) &&
+      ((chatId.match(/(@broadcast)/g) &&
+        chatId.match(/(@broadcast)/g).length > 1) ||
+        !chatId.match(/^(\d+(\d)*@broadcast)$/g))
+    ) {
+      return WAPI.scope(
+        chatId,
+        true,
+        404,
+        'incorrect parameters! Use as an example: 0000000000@broadcast'
+      );
+    }
+
+    if (
+      grup === chatId.substr(-grup.length, grup.length) &&
+      chatId.match(/(@g.us)/g) &&
+      chatId.match(/(@g.us)/g).length > 1
+    ) {
+      return WAPI.scope(
+        chatId,
+        true,
+        404,
+        'incorrect parameters! Use as an example: 00000000-000000@g.us or 00000000000000@g.us'
+      );
+    }
+  }
 }
 
 window.WAPI.sendExist = async function(chatId, returnChat = true, Send = true) {
-  // Check chat exists (group is always a chat)
-  let chat = await window.WAPI.getChat(chatId);
+  const checkType = WAPI.sendCheckType(chatId);
+  if (!!checkType && checkType.status === 404) {
+    return checkType;
+  }
+  let ck = await window.WAPI.checkNumberStatus(chatId, false);
 
-  // Check if contact number exists
-  if (!chat && !chatId.includes('@g')) {
-    let ck = await window.WAPI.checkNumberStatus(chatId);
+  if (ck.status === 404 && !chatId.includes('@g.us')) {
+    return WAPI.scope(chatId, true, ck.status, 'The number does not exist');
+  }
 
-    if (!ck.numberExists) {
-      return WAPI.scope(chatId, true, ck.status, 'The number does not exist');
+  let chat =
+    ck && ck.id && ck.id._serialized
+      ? await window.WAPI.getChat(ck.id._serialized)
+      : undefined;
+
+  if (ck.numberExists && chat === undefined) {
+    var idUser = new window.Store.UserConstructor(chatId, {
+      intentionallyUsePrivateConstructor: true
+    });
+    chat = await Store.Chat.find(idUser);
+  }
+
+  if (!chat) {
+    const storeChat = await window.Store.Chat.find(chatId);
+    if (storeChat) {
+      chat =
+        storeChat && storeChat.id && storeChat.id._serialized
+          ? await window.WAPI.getChat(storeChat.id._serialized)
+          : undefined;
     }
+  }
 
-    // Load chat ID for non contact
-    await window.Store.Chat.find(ck.id);
+  if (!ck.numberExists && !chat.t && chat.isUser) {
+    return WAPI.scope(chatId, true, ck.status, 'The number does not exist');
+  }
 
-    chatId = ck.id._serialized;
-    chat = await window.WAPI.getChat(chatId);
+  if (!ck.numberExists && !chat.t && chat.isGroup) {
+    return WAPI.scope(
+      chatId,
+      true,
+      ck.status,
+      'The group number does not exist on your chat list, or it does not exist at all!'
+    );
+  }
+
+  if (
+    !ck.numberExists &&
+    !chat.t &&
+    chat.id &&
+    chat.id.user != 'status' &&
+    chat.isBroadcast
+  ) {
+    return WAPI.scope(
+      chatId,
+      true,
+      ck.status,
+      'The transmission list number does not exist on your chat list, or it does not exist at all!'
+    );
   }
 
   if (!chat) {
     return WAPI.scope(chatId, true, 404);
   }
+
   if (Send) {
-    await window.WAPI.sendSeen(chatId);
+    await window.Store.SendSeen(chat, false);
   }
+
   if (returnChat) {
     return chat;
   }
+
   return WAPI.scope(chatId, false, 200);
 }
+
 
 /**
  * This function sts the profile name of the number.
