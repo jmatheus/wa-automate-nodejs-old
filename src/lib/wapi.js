aaab8abb4112c39acec3563e64ec7b3817a95fc7
Julio Matheus Vaz <jmatheus.vaz@gmail.com>
2021-11-25 16:14:49 -0300

    fix: method

--- src/lib/wapi.js
+++ src/lib/wapi.js
@@ -2500,13 +2500,13 @@ window.WAPI.forwardMessages = async function (to, messages, skipMyMessages) {
  *
  * @param {string} chatId '000000000000@c.us'
  */
-window.WAPI.getNewMessageId = function (chatId) {
+window.WAPI.getNewMessageId = async function (chatId) {
   const chat = await WAPI.sendExist(chatId);
   if (chat.id) {
     const newMsgId = new Object();
     newMsgId.fromMe = true;
     newMsgId.id = await WAPI.getNewId().toUpperCase();
-    newMsgId.remote = new Store.WidFactory.createWid(chatId);
+    newMsgId.remote = new Store.WidFactory.createWid(chat.id._serialized);
     newMsgId._serialized = `${newMsgId.fromMe}_${newMsgId.remote}_${newMsgId.id}`;
     const Msgkey = new Store.MsgKey(newMsgId);
     return Msgkey;
