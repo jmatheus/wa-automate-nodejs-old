dcc1d2cdb815a26dff5e1fe34ca3209d747c1e57
Julio Matheus Vaz <jmatheus.vaz@gmail.com>
2022-01-21 20:17:23 -0300

    add: checkNumberStatus with beta version

--- dist/lib/wapi.js
+++ dist/lib/wapi.js
@@ -1640,40 +1640,78 @@ window.WAPI.clearChat = async function (id) {
 //}
 
 window.WAPI.checkNumberStatus = async function (id, conn = true) {
-  try {
+   try {
     const err = { error: 404 };
+    const connection = window.Store.State.Socket.state;
     const checkType = WAPI.sendCheckType(id);
     if (!!checkType && checkType.status === 404) {
-      Object.assign(err, { text: checkType.text });
+      Object.assign(err, {
+        text: checkType.text,
+        numberExists: null
+      });
       throw err;
     }
 
     if (conn === true) {
-      const connection = window.Store.State.Socket.state;
       if (connection !== 'CONNECTED') {
         Object.assign(err, {
           text: 'No connection with WhatsApp',
-          connection: connection
+          connection: connection,
+          numberExists: null
         });
         throw err;
       }
     }
 
-    const result = await Store.checkNumber(id);
-    if (!!result && typeof result === 'object') {
-      const data = {
-        status: 200,
-        numberExists: true,
-        id: result.wid
-      };
-      return data;
+    const checkBeta = document.querySelector('._3hcUV');
+    if (checkBeta && checkBeta.innerText === 'BETA') {
+      return await Store.checkNumberBeta(id)
+        .then((result) => {
+          if (!!result && typeof result === 'object') {
+            const data = {
+              status: 200,
+              numberExists: true,
+              id: result.wid
+            };
+            return data;
+          }
+          throw Object.assign(err, {
+            connection: connection,
+            numberExists: false,
+            text: `The number does not exist`
+          });
+        })
+        .catch((err) => {
+          if (err.text) {
+            throw err;
+          }
+          throw Object.assign(err, {
+            connection: connection,
+            numberExists: false,
+            text: err
+          });
+        });
     }
 
-    throw err;
+    const result = await Store.checkNumber.queryExist(id);
+    if (result.status === 200) {
+      return {
+        status: result.status,
+        numberExists: true,
+        id: result.jid
+      };
+    }
+    return {
+      status: result.status,
+      numberExists: false,
+      text: `The number does not exist`
+    };
   } catch (e) {
     return {
       status: e.error,
-      text: e.text
+      text: e.text,
+      numberExists: e.numberExists,
+      connection: e.connection
     };
   }
 }
